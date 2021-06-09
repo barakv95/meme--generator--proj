d51daa35be4fb4f89814c22c2cc39c7c0ad756a3
Barak Vadei <barackv95@gmail.com>
2021-06-09 13:14:59 +0300

    add aditor area no func

--- js/service.js
+++ js/service.js
@@ -1,1 +1,26 @@
 `use strict`
+
+var keyWords = {
+    happy: 12,
+    funny: 1
+}
+
+var gImgs = [
+     {id:1,
+      url: "/meme-imgs (square)/1.jpg",
+      keyWords:['happy' , 'angry']}
+    ];
+    
+var gMeme = {
+    selectedImg: 5,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'Hello user',
+            size: 20,
+            align: 'left',
+            color: 'red'
+        }
+    ] 
+};
+
